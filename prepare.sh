3e6a0cc0ede79b59055040a5feebbe69b01ab7c1
Fernando Seiti Furusato <ferseiti@gmail.com>
2020-01-03 22:36:16 -0300

    Fixing preparation bash script
    Signed-off-by: Fernando Seiti Furusato <ferseiti@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-S=32
+S=64
 
     for i in {0000..0003}; do
-        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/'test'/${i}.h5 
+        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/'test'/${i}.h5 -b 21
     done &
 
     for i in {0004..0007}; do
-        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/validation/${i}_validation.h5 
+        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/validation/${i}_validation.h5 -b 21
     done &
 
     for i in {0008..0011}; do
-        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h5
+        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h -b 215
     done &
 
     for i in {0012..0015}; do
-        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h5
+        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h -b 215
     done &
 
     for i in {0016..0019}; do
-        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h5
+        python3 prepare_data.py --path /ddn/beamline/Fernando/upscaling/talitas/data/${i}/ -s $S -p $S -o /ddn/beamline/Fernando/upscaling/talitas/data/train/${i}.h -b 215
     done
